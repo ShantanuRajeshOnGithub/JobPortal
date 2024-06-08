e8371744b6a467394160a32df60170de8ca7bb16
Sathish Kumar <pspsathish@yahoo.com>
2024-06-08 20:50:23 +0530

    mongo deployment update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,25 +1,29 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+const options: MongoClientOptions = {
+  tls: true,
+  tlsAllowInvalidCertificates: true
+};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-const uri: string = process.env.MONGODB_URI;
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable to preserve the client across module reloads
+  // In development mode, use a global variable to preserve MongoClient instance across hot-reloads
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 

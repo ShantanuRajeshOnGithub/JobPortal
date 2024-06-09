28eb0077d2f4c072ed54232dbaa74460f89da958
Sathish Kumar <pspsathish@yahoo.com>
2024-06-09 19:24:23 +0530

    new pc commit

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -8,7 +8,7 @@ export async function getUserList() {
     const users = await db
       .collection("users")
       .find({})
-      .limit(10)
+      .limit(2)
       .toArray();
 
     console.log(users);

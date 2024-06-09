32d57ccfcca75f3b769593798471aa97971df452
Sathish Kumar <pspsathish@yahoo.com>
2024-06-09 23:31:47 +0530

    Update Check

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -8,7 +8,7 @@ export async function getUserList() {
     const users = await db
       .collection("users")
       .find({})
-      .limit(3)
+      .limit(5)
       .toArray();
 
     console.log(users);
